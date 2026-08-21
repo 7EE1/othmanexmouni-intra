e437a758485cf5db8393668aadf86fdef82bb265
7EE1 Student <7ee1@7ee1.org>
2026-08-21 19:37:51 +0100

    Complete C Fundamentals exercise

--- C00/ex02/main.c
+++ C00/ex02/main.c
@@ -5,7 +5,8 @@ int main()
     int a = 5;
     int b = 3;
     int sum = a + b;
-    printf("The sum of %d and %d is %d", a, b, sum);
+
+    printf("The sum of %d and %d is %d\n", a, b, sum);
 
     return 0;
 }
